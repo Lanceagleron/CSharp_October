6ac6236deaae04b4e1e4bb300f21e8a7835bc4a1
Lanceagleron <lanceagleron17@gmail.com>
2022-09-27 00:04:41 -0700

    studying csharp fundamentals

---
+++ assignments/Fundamentals1/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+// int start = 1;
+// int end = 255;
+// while (start <= end)
+// {
+//     Console.WriteLine(start);
+//     start = start + 1;
+// }
+
+Console.WriteLine("**************");
+
+string fizz = "fizz";
+string buzz = "buzz";
+
+for (int i = 1; i<=100; i++)
+{
+    if (i % 3 == 0)
+    {
+        Console.WriteLine(fizz);
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine(buzz);
+    }
+    else
+    Console.WriteLine(i);
+}
+
--- lecture/csharp_fundamentals/Program.cs
+++ lecture/csharp_fundamentals/Program.cs
@@ -79,3 +79,109 @@ while (x < 6)
 }
 
 Console.WriteLine("**************");
+
+//array
+// Declaring an array of length 5, initialized by default to all zeroes
+int[] numArray = new int[5];
+// Declaring an array with pre-populated values
+// For Arrays initialized this way, the length is determined by the size of the given data
+int[] numArray2 = {1,2,3,4,6};
+
+//Accessing Arrays
+//Arrays are zero-indexed. This means that the first value within an array is stored at index 0. The second value within an Array resides at index 1, and so on.  
+
+int[] arrayOfInts = {1, 2, 3, 4, 5};
+Console.WriteLine(arrayOfInts[0]);    // The first number lives at index 0.
+Console.WriteLine(arrayOfInts[1]);    // The second number lives at index 1.
+Console.WriteLine(arrayOfInts[2]);    // The third number lives at index 2.
+Console.WriteLine(arrayOfInts[3]);    // The fourth number lives at index 3.
+Console.WriteLine(arrayOfInts[4]);    // The fifth and final number lives at index 4.
+
+Console.WriteLine("**************");
+
+string[] myCars = new string[] { "Mazda Miata", "Ford Model T", "Dodge Challenger", "Nissan 300zx"};
+// The 'Length' property tells us how many values are in the Array (4 in our example here). 
+// We can use this to determine where the loop ends: Length-1 is the index of the last value. 
+for (int idx = 0; idx < myCars.Length; idx++)
+{
+    Console.WriteLine($"I own a {myCars[idx]}");
+}
+
+Console.WriteLine("**************");
+
+//foreach
+foreach (string car in myCars)
+{
+    // We no longer need the index, because variable 'car' already represents each indexed value
+    Console.WriteLine($"I own a {car}");
+}
+
+Console.WriteLine("**************");
+
+//list
+//Initializing an empty list of Motorcycle Manufacturers
+List<string> bikes = new List<string>();
+//Use the Add function in a similar fashion to push
+bikes.Add("Kawasaki");
+bikes.Add("Triumph");
+bikes.Add("BMW");
+bikes.Add("Moto Guzzi");
+bikes.Add("Harley Davidson");
+bikes.Add("Suzuki");
+//Accessing a generic list value is the same as you would an array
+Console.WriteLine(bikes[2]); //Prints "BMW"
+Console.WriteLine($"We currently know of {bikes.Count} motorcycle manufacturers.");
+
+Console.WriteLine("**************");
+
+//itereate through a list 
+//Using our list of motorcycle manufacturers from before
+//we can easily loop through the list of them with a C-style for loop
+//this time, however, Count is the attribute for a number of items.
+Console.WriteLine("The current manufacturers we have seen are:");
+for (var idx = 0; idx < bikes.Count; idx++)
+{
+    Console.WriteLine("-" + bikes[idx]);
+}
+// Insert a new item between a specific index
+bikes.Insert(2, "Yamaha");
+//Removal from Generic List
+//Remove is a lot like Javascript array pop, but searches for a specified value
+//In this case we are removing all manufacturers located in Japan
+bikes.Remove("Suzuki");
+bikes.Remove("Yamaha");
+bikes.RemoveAt(0); //RemoveAt has no return value however
+//The updated list can then be iterated through using a foreach loop
+Console.WriteLine("List of Non-Japanese Manufacturers:");
+foreach (string manu in bikes)
+{
+    Console.WriteLine("-" + manu);
+}
+
+Console.WriteLine("**************");
+
+//dictionary
+Dictionary<string,string> profile = new Dictionary<string,string>();
+//Almost all the methods that exists with Lists are the same with Dictionaries
+profile.Add("Name", "Speros");
+profile.Add("Language", "PHP");
+profile.Add("Location", "Greece");
+Console.WriteLine("Instructor Profile");
+Console.WriteLine("Name - " + profile["Name"]);
+Console.WriteLine("From - " + profile["Location"]);
+Console.WriteLine("Favorite Language - " + profile["Language"]);
+
+Console.WriteLine("**************");
+
+foreach (KeyValuePair<string,string> entry in profile)
+{
+    Console.WriteLine(entry.Key + " - " + entry.Value);
+}
+
+Console.WriteLine("**************");
+
+//The var keyword takes the place of a type in type-inference
+foreach (var entry in profile)
+{
+    Console.WriteLine(entry.Key + " - " + entry.Value);
+}
